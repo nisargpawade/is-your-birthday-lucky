ad31b387b576b4dd98030acb15f0b7899a7042ad
nisargpawade <70917826+nisargpawade@users.noreply.github.com>
2021-09-10 18:16:30 +0530

    done with logic

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const birthDate = document.querySelector("#birthdate");
 const luckyNo = document.querySelector("#lucky-no");
 const checkBtn = document.querySelector("#check-btn");
+const resultMessage = document.querySelector("#result-msg");
+
+function showMsg(msg){
+    resultMessage.innerText = msg;
+    resultMessage.style.display = "block";
+}
+
+function hideMsg(){
+    resultMessage.style.display = "none";
+}
+
+function calculateSum(birthDate){
+    const birthNo = birthDate.value.replaceAll('-', '');
+    var sum = 0;
+    for (let i=0; i<birthNo.length; i++) {
+        sum = sum + Number(birthNo.charAt(i))
+    };
+    return sum
+}
+
+function checkLucky(birthDate, luckyNo) {
+    hideMsg()
+    if (Number(calculateSum(birthDate))%Number(luckyNo.value) == 0) {
+        console.log("you are lucky");
+        showMsg("You are lucky");
+    } 
+    else {
+        showMsg("You are Unlucky");
+    console.log("you are unlucky")
+}
+};
 
 checkBtn.addEventListener("click", function clickHandler() {
-    console.log(birthDate.value);
-    console.log(luckyNo.value);
+    checkLucky(birthDate, luckyNo);
 })
